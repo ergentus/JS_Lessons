3fa7b282221b24a0e64253a3d235e87aae92843e
ergentus <ponfable999@gmail.com>
2023-06-20 04:01:01 +0300

    lesson4

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
-import './lesson_4';
+import './lesson_4'
+import {handlePromise} from './lesson_4'
+
 
 const Lesson4 = () => {
-    return (
-        <div>
-            <button id='btn-create-promise'>Create Promise</button>
-            <button id='btn-resolve-promise'>Resolve Promise</button>
-            <button id='btn-reject-promise'>Reject Promise</button>
-        </div>
-    );
+	const createPromise = () => {
+		handlePromise.promise = new Promise((resolve, reject) => {
+			handlePromise.resolve = resolve
+			handlePromise.reject = reject
+		})
+		handlePromise.promise
+			.then(handlePromise.onSuccess)
+			.catch(handlePromise.onError)
+
+		window.test = handlePromise
+	}
+
+	const resolvePromise = () => {
+		handlePromise.resolve && handlePromise.resolve('resolved data')
+	}
+	const rejectPromise = () => {
+		handlePromise.reject && handlePromise.reject('error message')
+	}
+
+	return (
+		<div>
+			<button id="btn-create-promise" onClick={createPromise}>Create Promise</button>
+			<button id="btn-resolve-promise" onClick={resolvePromise}>Resolve Promise</button>
+			<button id="btn-reject-promise" onClick={rejectPromise}>Reject Promise</button>
+		</div>
+	)
 }
 
-export default Lesson4;
+export default Lesson4
--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,5 +1,3 @@
-console.log('lesson 4');
-
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
 
@@ -8,25 +6,33 @@ console.log('lesson 4');
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
 
+import {log} from 'util'
+
+const promise = new Promise(() => {
+})
+console.log('Promise is created', promise)
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
-
+const promise2 = new Promise((res) => res('Promise Data'))
+	.then(data => console.log(data, '2'))
 
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
-
+const promise3 = new Promise((res, rej) => rej('Promise Error')).catch(err => console.log(err, '3'))
 
 // Task 04
 // Создайте промис, который переходит в состояние resolved через 3с.
 // (Используйте setTimeout)
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
-
+const promise4 = new Promise((res) => {
+	setTimeout(() => res('Promise Data'), 4)
+}).then(data => console.log(data, '4'))
 
 // Task 05
 // Создайте литерал объекта handlePromise со следующими свойствами:
@@ -40,6 +46,17 @@ console.log('lesson 4');
 // описаного выше объекта: свойство promise получает новый созданный промис,
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
+export const handlePromise: any = {
+	promise: null,
+	resolve: null,
+	reject: null,
+	onSuccess(paramName: string) {
+		console.log(`Promise is resolved with data: ${paramName}`)
+	},
+	onError(paramName: string) {
+		console.log(`Promise is rejected with error: ${paramName}`)
+	},
+}
 
 
 // Task 06
@@ -49,6 +66,20 @@ console.log('lesson 4');
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
 
+const promise6 = new Promise((resolve) => {
+	setTimeout(() => resolve('My name is'), 1000)
+})
+promise6
+	.then(onSuccess)
+	.then(print)
+
+function onSuccess(name: any) {
+	return name + ' Oleg'
+}
+
+function print(myLog: any) {
+	console.log(myLog)
+}
 
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
@@ -56,7 +87,20 @@ console.log('lesson 4');
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
 
+const firstPr = new Promise((resolve) => {
+	setTimeout(() => resolve({name: 'Anna'}), 2000)
+})
+const secondPr = new Promise((resolve) => {
+	setTimeout(() => resolve({age: 16}), 3000)
+})
+const thirdPr = new Promise((resolve) => {
+	setTimeout(() => resolve({city: ''}), 4000)
+})
 
-
+Promise.all([firstPr, secondPr, thirdPr])
+	.then(resolve => {
+		console.log(Object.assign({}, ...resolve))
+	}).catch(err => console.error(err))
 // just a plug
-export default ()=>{};
+export default () => {
+};
